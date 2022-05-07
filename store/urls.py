6ad0bb99317513f5fc5851d02590d1129411a8b1
edupajeu <edupajeu@gmail.com>
2022-05-07 23:41:28 +0100

    - Implement Product Detail Url and Design

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.store, name='store'),
     path('<slug:category_slug>/', views.store, name='products_by_category'),  # URL prduct by category
+    path('<slug:category_slug>/<slug:product_slug>/', views.product_detail,
+         name='product_detail'),  # URL prduct by category
 
 ]
