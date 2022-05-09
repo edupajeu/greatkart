efd404a763a789312e34721f786c313b48550116
edupajeu <edupajeu@gmail.com>
2022-05-09 11:43:39 +0100

    - Single Product View

--- store/views.py
+++ store/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
@@ -21,5 +22,13 @@ def store(request, category_slug=None):
     return render(request, 'store/store.html', context)
 
 def product_detail(request, category_slug, product_slug):
-    return render(request, 'store/product_detail.html')
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    
+    context = {
+        'single_product': single_product,
+    }
+    return render(request, 'store/product_detail.html', context)
 
