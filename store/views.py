154a43e1d561f80d6755c39cbbbdbea1a80b3a32
edupajeu <edupajeu@gmail.com>
2022-05-09 12:18:27 +0100

    - Get URL for Product - Include floatformat to float fields

--- store/views.py
+++ store/views.py
@@ -21,7 +21,7 @@ def store(request, category_slug=None):
     }
     return render(request, 'store/store.html', context)
 
-def product_detail(request, category_slug, product_slug):
+def product_detail(request, category_slug, product_slug): # Get all product details by slug
     try:
         single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
     except Exception as e:
