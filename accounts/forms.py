3857ae53d7dae41a281308a670d33616e03701ec
edupajeu <edupajeu@gmail.com>
2022-09-18 15:46:30 +0100

    - Dashboard Edits - My Orders - User Profile Model - Edit Profile Setup - Edit Profile Functionality - Automatically Create User Profile Change Password - Order Detail Page - Fix Profile Picture in Dashboard

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Account
+from .models import Account, UserProfile
 
 
 # Register form
@@ -15,15 +15,6 @@ class RegistrationForm(forms.ModelForm):
         model = Account
         fields = ['first_name', 'last_name', 'email', 'phone_number', 'password']
 
-    def __init__(self, *args, **kwargs):  # It will define the specific attribute for each field
-        super(RegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
-        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
-        self.fields['email'].widget.attrs['placeholder'] = 'Enter e-mail Address'
-        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
-        for field in self.fields:
-            self.fields[field].widget.attrs['class'] = 'form-control'
-
     def clean(self):
         clean_data = super(RegistrationForm, self).clean()
         password = clean_data.get('password')
@@ -33,3 +24,37 @@ class RegistrationForm(forms.ModelForm):
             raise forms.ValidationError(
                 "Password does not match!"
             )
+
+    def __init__(self, *args, **kwargs):  # It will define the specific attribute for each field (CSS class)
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter e-mail Address'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone Number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('first_name', 'last_name', 'phone_number')
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+
+class UserProfileForm(forms.ModelForm):
+    profile_picture = forms.ImageField(required=False, error_messages={"invalid": "Image files only"},
+                                       widget=forms.FileInput)
+
+    class Meta:
+        model = UserProfile
+        fields = ('address_line_1', 'address_line_2', 'city', 'state', 'country', 'profile_picture')
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,13 @@ from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 import requests
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from carts.models import Cart, CartItem
 from carts.views import _cart_id
-from .forms import RegistrationForm
-from .models import Account
+from orders.models import Order, OrderProduct
+from .forms import RegistrationForm, UserForm, UserProfileForm
+from .models import Account, UserProfile
 
 # Email Verification libraries
 from django.contrib.auth.tokens import default_token_generator
@@ -18,6 +19,7 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
 
+# Registration section
 def register(request):
     if request.method == 'POST':
         form = RegistrationForm(request.POST)
@@ -32,6 +34,13 @@ def register(request):
                                                username=username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            # Create User Profile
+            profile = UserProfile()
+            profile.user_id = user.id
+            profile.profile_picture = 'default/default-user.png'
+            profile.save()
+
             # TODO: Fix the send mail functionality
             # User Activation by Email
             current_site = get_current_site(request)
@@ -54,6 +63,7 @@ def register(request):
     return render(request, 'accounts/register.html', context)
 
 
+# Login section
 def login(request):
     if request.method == 'POST':
         email = request.POST.get('email')
@@ -121,6 +131,7 @@ def login(request):
     return render(request, 'accounts/login.html')
 
 
+# Logout
 @login_required(login_url='login')
 def logout(request):
     auth.logout(request)
@@ -146,11 +157,21 @@ def activate(request, uidb64, token):
         return redirect('register')
 
 
+# Dashboard
 @login_required(login_url='login')
 def dashboard(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Order.objects.order_by('-created_at').filter(user_id=request.user.id, is_ordered=True)
+    orders_count = orders.count()
+
+    userprofile = UserProfile.objects.get(user_id=request.user.id)
+    context = {
+        'orders_count': orders_count,
+        'userprofile': userprofile,
+    }
+    return render(request, 'accounts/dashboard.html', context)
 
 
+# Forgot Password
 def forgotPassword(request):
     if request.method == 'POST':
         email = request.POST.get('email')
@@ -195,6 +216,7 @@ def resetpassword_validate(request, uidb64, token):
         return redirect('login')
 
 
+# Reset password
 def resetPassword(request):
     if request.method == 'POST':
         password = request.POST.get('password')
@@ -212,3 +234,81 @@ def resetPassword(request):
             return redirect('resetPassword')
     else:
         return render(request, 'accounts/resetPassword.html')
+
+
+# My Orders
+@login_required(login_url='login')
+def my_orders(request):
+    orders = Order.objects.filter(user=request.user, is_ordered=True).order_by('-created_at')  # The hyphen is used to retrive the result in descending order
+    context = {
+        'orders': orders
+    }
+    return render(request, 'accounts/my_orders.html', context)
+
+
+# Edit profile validation form
+@login_required(login_url='login')
+def edit_profile(request):
+    userprofile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)  # The instance ensure the unique profile
+        profile_form = UserProfileForm(request.POST, request.FILES, instance=userprofile)  # request.FILES is required for the file uploaded
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile has been saved successfully.')
+            return redirect('edit_profile')
+
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = UserProfileForm(instance=userprofile)
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form,
+        'userprofile': userprofile,
+    }
+    return render(request, 'accounts/edit_profile.html', context)
+
+
+# Change password
+@login_required(login_url='login')
+def change_password(request):
+    if request.method == 'POST':
+        current_password = request.POST['current_password']
+        new_password = request.POST['new_password']
+        confirm_password = request.POST['confirm_password']
+
+        user = Account.objects.get(username__exact=request.user.username)
+
+        if new_password == confirm_password:
+            success = user.check_password(current_password)
+            if success:
+                user.set_password(new_password)
+                user.save()
+                # auth.logout(request) could be called if you would like to log out
+                messages.success(request, "Your password has been changed")
+                return redirect('change_password')
+            else:
+                messages.error(request, "Please enter a valid current password")
+                return redirect('change_password')
+        else:
+            messages.error(request, "Password does not match")
+            return redirect('change_password')
+    return render(request, 'accounts/change_password.html')
+
+
+# Order Details
+@login_required(login_url='login')
+def order_detail(request, order_id):
+    order_detail = OrderProduct.objects.filter(order__order_number=order_id)
+    order = Order.objects.get(order_number=order_id)
+    subtotal = 0
+    for i in order_detail:
+        subtotal += i.product.price * i.quantity
+    context = {
+        'order_detail': order_detail,
+        'order': order,
+        'subtotal': subtotal,
+    }
+    return render(request, 'accounts/order_detail.html', context)
